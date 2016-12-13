a70594342f336ced874e18bee81ea4581333a746
Alvaro Carneiro <alv.ccbb@gmail.com>
2016-12-13 10:43:54 -0300

    Before this change, it would only work when the page was loaded being at the top

--- Sticky.js
+++ Sticky.js
@@ -19,14 +19,20 @@
 
 		this.options = options || {};
 
+		var new_stick;
+
 		for (var i = 0; i < elem.length; i++) {
 			var element = elem[i];
 
-			sticky.sticked.push({
+			new_stick = {
 				node: element,
 				rect: element.getBoundingClientRect(),
 				sticked: false
-			});
+			};
+
+			new_stick.top = new_stick.rect.top+window.scrollY;
+
+			sticky.sticked.push(new_stick);
 		}
 
 		window.addEventListener('scroll', function() {
@@ -34,7 +40,7 @@
 				var element = sticky.sticked[i], y = window.scrollY + (sticky.options['margin']||0);
 
 
-				if (y >= element.rect.top && ! element.sticked) {
+				if (y >= element.top && ! element.sticked) {
 
 					element.node.style.width = element.rect.width+'px';
 					element.node.style.position = 'fixed';
@@ -48,7 +54,7 @@
 					element.sticked = true;
 
 				}
-				else if (y < element.rect.top && element.sticked) {
+				else if (y < element.top && element.sticked) {
 					element.node.style.position = 'static';
 					element.node.style.top = 'auto';
 
